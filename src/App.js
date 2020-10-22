871b14bc6c2f75523491a9724ab23ee672702f8c
chrishargan <chrishargan@gmail.com>
2020-10-22 16:29:55 +0200

    fixed problem with undefned persons component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,103 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+    state = {
+        persons: [
+            { id: 'asfa1', name: 'Max', age: 28 },
+            { id: 'vasdf1', name: 'Manu', age: 29 },
+            { id: 'asdf11', name: 'Stephanie', age: 26 }
+        ],
+        otherState: 'some other value',
+        showPersons: false
+    }
+
+    nameChangedHandler = ( event, id ) => {
+        const personIndex = this.state.persons.findIndex(p => {
+            return p.id === id;
+        });
+
+        const person = {
+            ...this.state.persons[personIndex]
+        };
+
+        // const person = Object.assign({}, this.state.persons[personIndex]);
+
+        person.name = event.target.value;
+        const persons = [...this.state.persons];
+        persons[personIndex] = person;
+
+        this.setState( {persons: persons} );
+    }
+
+    deletePersonHandler = (personIndex) => {
+        // const persons = this.state.persons.slice();
+        const persons = [...this.state.persons];
+        persons.splice(personIndex, 1);
+        this.setState({persons: persons});
+    }
+
+    togglePersonsHandler = () => {
+        const doesShow = this.state.showPersons;
+        this.setState( { showPersons: !doesShow } );
+    }
+
+    render () {
+        const style = {
+            backgroundColor: 'white',
+            font: 'inherit',
+            border: '1px solid blue',
+            padding: '8px',
+            cursor: 'pointer'
+        };
+
+        let persons = null;
+
+        if ( this.state.showPersons ) {
+            persons = (
+                <div>
+                    {this.state.persons.map((person, index) => {
+                        return <Person
+                            click={() => this.deletePersonHandler(index)}
+                            name={person.name}
+                            age={person.age}
+                            key={person.id}
+                            changed={(event) => this.nameChangedHandler(event, person.id)} />
+                    })}
+                </div>
+            );
+        }
+
+        return (
+            <div className="App">
+                <h1>Hi, I'm a React App</h1>
+                <p>This is really working!</p>
+                <button
+                    style={style}
+                    onClick={this.togglePersonsHandler}>Toggle Persons</button>
+                {persons}
+            </div>
+        );
+        // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
+    }
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 import React, {Component} from 'react';
 import './App.css';
 import Person from './Person/Person';
@@ -40,7 +140,7 @@ class App extends Component {
         this.setState({persons: persons})
     }
 
-    /* the toggleHandler method setState targets the showPersons and swaps to the opposite of what doesShow is     */
+    /!* the toggleHandler method setState targets the showPersons and swaps to the opposite of what doesShow is     *!/
     togglePersonsHandler = () => {
         const doesShow = this.state.showPersons;
         this.setState({showPersons: !doesShow});
@@ -76,7 +176,7 @@ class App extends Component {
                         name={this.state.persons[2].name}
                         age={this.state.persons[2].age}/>
 
-                    { /* above is how to hard code something in JSX when what is preferable is to use the mapped method at top */}
+                    { /!* above is how to hard code something in JSX when what is preferable is to use the mapped method at top *!/}
                 </div>
             );
         }
@@ -89,7 +189,8 @@ class App extends Component {
                     onClick={this.togglePersonsHandler}>Toggle Persons
                 </button>
                 {persons}
-                { this.state.showPersons ?
+                {
+                    this.state.showPersons ?
                     <div>
                         <Person
                             name={this.state.persons[0].name}
@@ -108,12 +209,12 @@ class App extends Component {
 
             </div>
         );
-        /* you cannot have block js code in JSX blocks but you
+        /!* you cannot have block js code in JSX blocks but you
                    can use terniary statements like ? instead of if statement checking something to be true and the : at the end to imply else
                    so here it checks if showPersons is true then render the components content else render null
-                    */
+                    *!/
         // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
     }
 }
 
-export default App;
+export default App;*/
